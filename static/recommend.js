55f5bb819a572efc3c633f096c6d9b045ca82752
Joseff Tan <66125884+ijoseff@users.noreply.github.com>
2023-10-12 02:31:05 -0700

    Update recommend.js

--- static/recommend.js
+++ static/recommend.js
@@ -27,7 +27,7 @@ $(function() {
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
   $("#loader").fadeIn();
-  var my_api_key = '9b972023c0d';
+  var my_api_key = '9b972023c0d53842f4fe350c3567e72d';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
